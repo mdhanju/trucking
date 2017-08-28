4cdc33d5d51c34214c9a0cf0bd84a8f50472ce41
Maninder Dhanju <maninderdhanju@Maninders-MacBook-Pro.local>
2017-08-27 22:48:27 -0700

    Updates license values

--- data/license.js
+++ data/license.js
@@ -16,35 +16,35 @@ export const classifications = [{
 ]
 
 export const endorsements = [{
-		value: 'T – Double/Triples',
+		value: 'T',
 		label: 'T – Double/Triples',
 		detail: '“T” endorsement is required if the vehicle being driven requires a Class A CDL  and is towing more than one trailer.'
 	}, {
-		value: 'P – Passenger/Transportation',
+		value: 'P',
 		label: 'P – Passenger/Transportation',
 		detail: '“P” endorsement is required if the vehicle being driven requires a Class A, B or C CDL and is transporting passengers. Applicants or holders of a public passenger endorsement must have an acceptable driving record. '
 	}, {
-		value: 'N – Liquid Bulk/Tank Cargo',
+		value: 'N',
 		label: 'N – Liquid Bulk/Tank Cargo',
 		detail: '“N” endorsement  is required if the vehicle being driven requires a Class A or B CDL and is designed to haul a liquid or liquid gas in a permanently mounted cargo tank rated at 119 gallons or more or a portable tank rated at 1,000 gallons or more. A tank endorsement is also required for Class C vehicles when the vehicle is used to transport hazardous materials in liquid or gas form in the above described rated tanks.'
 	},
 	{
-		value: 'H – Hazardous Material',
+		value: 'H',
 		label: 'H – Hazardous Material',
 		detail: '“H” endorsement is required if the vehicle being driven is transporting hazardous materials which are placarded. You must be a U.S. citizen or legal permanent resident to obtain a hazardous material endorsement. '
 	},
 	{
-		value: 'X – Hazardous Material and Tank, Combined',
+		value: 'X',
 		label: 'X – Hazardous Material and Tank, Combined',
 		detail: '“X” endorsement is required if the vehicle being driven is transporting hazardous materials via a tank.  You must be a U.S. citizen or legal permanent resident to obtain a hazardous material endorsement. '
 	},
 	{
-		value: 'S – School Bus',
+		value: 'S',
 		label: 'S – School Bus',
 		detail: '“S” endorsement is required before operating a school bus.  “P” endorsement is also required.  Applicants or holders of a public passenger endorsement must have an acceptable driving record.'
 	},
 	{
-		value: 'V – Student Transportation Vehicle',
+		value: 'V',
 		label: 'V – Student Transportation Vehicle',
 		detail: ' “V” endorsement is required for operation of a student transportation vehicle; transporting students to and from school, including vehicles transporting special education students. Applicants or holders of a public passenger endorsement must have an acceptable driving record.'
 	},
@@ -54,99 +54,99 @@ export const endorsements = [{
 		detail: '“A” endorsement is required prior to operating a student transportation vehicle (or other vehicle that requires a “F” endorsement) used in connection with school sponsored events and activities, but not used to transport students to and from school.  Applicants or holders of a public passenger endorsement must have an acceptable driving record. '
 	},
 	{
-		value: 'F – Taxi, Livery, Service Bus, Motor Bus or Motor Coach',
+		value: 'F',
 		label: 'F – Taxi, Livery, Service Bus, Motor Bus or Motor Coach',
 		detail: '“F” endorsement is required for operation of a taxi, livery vehicle, service bus, motor bus or motor coach. Applicants or holders of a public passenger endorsement must have an acceptable driving record. '
 	},
 ]
 
-export const Restrictions = [{
-		value: '3 = With "M" endorsement allows operation of three-wheeled motorcycle.',
+export const restrictions = [{
+		value: '3',
 		label: '3 = With "M" endorsement allows operation of three-wheeled motorcycle.',
 		detail: ''
 	},
 	{
-		value: 'B = Corrective lenses',
+		value: 'B',
 		label: 'B = Corrective lenses',
 		detail: ''
 	},
 	{
-		value: 'C = Mechanical Aid',
+		value: 'C',
 		label: 'C = Mechanical Aid',
 		detail: ''
 	},
 	{
-		value: 'D = Prosthetic Aid',
+		value: 'D',
 		label: 'D = Prosthetic Aid',
 		detail: ''
 	},
 	{
-		value: 'E = Automatic Transmission only',
+		value: 'E',
 		label: 'E = Automatic Transmission only',
 		detail: ''
 	},
 	{
-		value: 'F = Duel Outside Mirror',
+		value: 'F',
 		label: 'F = Duel Outside Mirror',
 		detail: ''
 	},
 	{
-		value: 'G = Daylight only operation',
+		value: 'G',
 		label: 'G = Daylight only operation',
 		detail: ''
 	},
 	{
-		value: 'K = CDL Intrastate only',
+		value: 'K',
 		label: 'K = CDL Intrastate only',
 		detail: ''
 	},
 	{
-		value: 'L = Vehicles without air brakes',
+		value: 'L',
 		label: 'L = Vehicles without air brakes',
 		detail: ''
 	},
 	{
-		value: 'M = Group B or Group C passenger vehicles',
+		value: 'M',
 		label: 'M = Group B or Group C passenger vehicles',
 		detail: ''
 	},
 	{
-		value: 'N  = Group C passenger vehicles only',
+		value: 'N',
 		label: 'N  = Group C passenger vehicles only',
 		detail: ''
 	},
 	{
-		value: 'O = No fifth wheel (Combination vehicles)',
+		value: 'O',
 		label: 'O = No fifth wheel (Combination vehicles)',
 		detail: ''
 	},
 	{
-		value: 'P = Commercial Learners Permit Only - No passengers other than trainer, additional trainees or test examiner.',
+		value: 'P',
 		label: 'P = Commercial Learners Permit Only - No passengers other than trainer, additional trainees or test examiner.',
 		detail: ''
 	},
 	{
-		value: 'U = Hearing aide required',
+		value: 'U',
 		label: 'U = Hearing aide required',
 		detail: ''
 	},
 	{
-		value: 'V = Medical variance required',
+		value: 'V',
 		label: 'V = Medical variance required',
 		detail: ''
 	},
 	{
-		value: 'W = Medical waiver required',
+		value: 'W',
 		label: 'W = Medical waiver required',
 		detail: ''
 	},
 	{
-		value: 'X = Commercial Learners Permit Only - No cargo in tank vehicle and tank must be purged. Restricts "N" endorsement.',
+		value: 'X',
 		label: 'X = Commercial Learners Permit Only - No cargo in tank vehicle and tank must be purged. Restricts "N" endorsement.',
 		detail: ''
 	},
 	{
-		value: 'Z = No full air brake equipped commercial motor vehicle.',
+		value: 'Z',
 		label: 'Z = No full air brake equipped commercial motor vehicle.',
 		detail: ''
 	}
